1499c7a38856d7293e63d8d59daf6982a24d4e10
Eduardo <ehallabreu@gmail.com>
2026-01-13 17:56:23 -0800

    commenting for pretty

--- analysis.py
+++ analysis.py
@@ -1,17 +1,22 @@
 import json
 
-with open('ucaccmet2j_python/precipitation.json') as file:
+# loading data
+with open('precipitation.json') as file:
     data = json.load(file)
 
+# function to get total monthly precipitation, yearly precipitation and relative monthly precipitation given the station
 def get_data(station_code):
+    
+    # filter for station
     station = []
     for dict in data: # 
         if dict['station'] == station_code:
             station.append(dict)
 
     monthly_precipitation = {}
-    yearly_precipitation = 0
-
+    total_yearly_precipitation = 0
+    
+    # compute total monthly precipitation and total yearly precipitation
     for dict in station: 
         date = dict['date']
         month = date.split('-')[1]
@@ -21,13 +26,13 @@ def get_data(station_code):
         else:
             monthly_precipitation[month] = dict['value']
 
-        yearly_precipitation += dict['value']
+        total_yearly_precipitation += dict['value']
 
     total_monthly_percipitation = list(monthly_precipitation.values())
-    total_yearly_precipitation = yearly_precipitation
 
     relative_precipitation = []
     
+    # compute relative precipitation
     for month in total_monthly_percipitation:
         relative_month = month / total_yearly_precipitation
         relative_precipitation.append(relative_month)
@@ -39,17 +44,19 @@ cincinnati_code = 'GHCND:USW00093814'
 maui_code = 'GHCND:USC00513317'
 san_diego_code = 'GHCND:US1CASD0032'
 
+# call the function for each station
 seatle_monthly, seatle_yearly, seatle_relative = get_data(seatle_code)
 cincinnati_monthly, cincinnati_yearly, cincinnati_relative = get_data(cincinnati_code)
 maui_monthly, maui_yearly, maui_relative = get_data(maui_code)
 san_diego_monthly, san_diego_yearly, san_diego_relative = get_data(san_diego_code)
 
+# compute relative yearly precipitation
 seatle_relative_yearly = seatle_yearly / (seatle_yearly + cincinnati_yearly + maui_yearly + san_diego_yearly)
 cincinnati_relative_yearly = cincinnati_yearly / (seatle_yearly + cincinnati_yearly + maui_yearly + san_diego_yearly)
 maui_relative_yearly = maui_yearly / (seatle_yearly + cincinnati_yearly + maui_yearly + san_diego_yearly)
 san_diego_relative_yearly = san_diego_yearly / (seatle_yearly + cincinnati_yearly + maui_yearly + san_diego_yearly)
 
-
+# save to a json file
 precipitation = {
     'Cincinnati': {
         'station': 'GHCND:USW00093814',
@@ -88,5 +95,5 @@ precipitation = {
     },
 }
 
-with open('ucaccmet2j_python/results.json', 'w', encoding='utf-8') as file:
+with open('results.json', 'w', encoding='utf-8') as file:
     json.dump(precipitation, file, indent=4)
